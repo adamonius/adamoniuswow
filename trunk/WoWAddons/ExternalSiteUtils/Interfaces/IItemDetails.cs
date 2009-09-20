11f7b0471bd9cf250502e0c04cddd0ccd097e6bc
adamonius <adamonius@005f0e14-a4b8-11de-bf1c-ef771b8a44ac>
2009-09-20 20:02:49 +0000

    AnkhSVN adding

---
+++ trunk/WoWAddons/ExternalSiteUtils/Interfaces/IItemDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ExternalSiteUtils
+{
+    public interface IItemDetails
+    {
+        Int32 ILvl { get; }
+        Int32 ItemID { get; }
+        String Name { get; }
+        ItemQuality Quality { get; }
+        Boolean IsTwoHandWeapon { get; }
+        String Tooltip { get; }
+    }
+}
